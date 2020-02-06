3fc1da3fd2001834cacc4bb70c7a5aa1c144bc46
caiyu <932475428@qq.com>
2020-02-06 10:43:21 +0800

    update jenkins

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import avator from './assets/images/avator.png';
 import './App.css';
-
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={avator} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload ----.
+          myBlog
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
